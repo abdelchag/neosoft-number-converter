ef4291d56b695f1e399a391c27d072aa552f7107
AbdallahCure51 <abdellah.chagdani@gmail.com>
2024-06-10 18:05:12 +0200

    NeoSoftService refacto

--- src/main/java/org/abdel/neosoft/interview/service/NeoSoftService.java
+++ src/main/java/org/abdel/neosoft/interview/service/NeoSoftService.java
@@ -1,29 +1,32 @@
 package org.abdel.neosoft.interview.service;
 
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class NeoSoftService {
 
-    private static final Map<Integer, String> CASES = Map.of(
+    private static final Map<Integer, String> CASES_CONTENT = Map.of(
             3, "Neo",
             5, "So",
             7, "Ft"
     );
 
+    private static final List<Integer> CASES_DIVIDER = Arrays.asList(3, 5);
+
 
     public String convertNumber(int inputNumber) {
         StringBuilder sb = new StringBuilder();
-        if (inputNumber % 3 == 0) {
-            sb.append(CASES.get(3));
-        }
-        if (inputNumber % 5 == 0) {
-            sb.append(CASES.get(5));
-        }
+
+        CASES_DIVIDER.stream()
+                .filter(d -> inputNumber % d == 0)
+                .forEach(d -> sb.append(CASES_CONTENT.get(d)));
+
         String inputString = String.valueOf(inputNumber);
         inputString.chars().map(Character::getNumericValue)
-                .filter(CASES::containsKey)
-                .forEach(c -> sb.append(CASES.get(c)));
+                .filter(CASES_CONTENT::containsKey)
+                .forEach(c -> sb.append(CASES_CONTENT.get(c)));
         if (sb.isEmpty()) {
             sb.append(inputString);
         }
